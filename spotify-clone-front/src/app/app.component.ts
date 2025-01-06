1f099256e3d352431d19a29ffb620bb22c28260c
diffonathan <152519229+diffonathan@users.noreply.github.com>
2025-01-06 11:03:11 +0100

    06-01-25

--- spotify-clone-front/src/app/app.component.ts
+++ spotify-clone-front/src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Component, inject, OnInit } from '@angular/core';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FontAwesomeModule], // Modifié
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
   title = 'spotify-clone-front';
+   
+  private faIconLibrary: FaIconLibrary = inject(FaIconLibrary);
+
+  private initFontAwesome(): void {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
---
+++ spotify-clone-front/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,6 @@
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+
+export const fontAwesomeIcons: IconDefinition[] = [
+    faCirclePlay
+];
